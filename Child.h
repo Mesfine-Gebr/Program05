c14f08a728e51394573b20c999b150da97589779
pmsese <120345253+pmsese@users.noreply.github.com>
2022-12-11 17:12:52 -0800

    Add files via upload

---
+++ Child.h
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+class Child
+{
+    public:
+        Child();
+        Child(string first, string last, int age);
+        ~Child();
+
+        bool operator <(const Child &c);
+        bool operator >(const Child &c);
+        bool operator ==(const Child &c);
+        bool operator !=(const Child &c);
+        friend ostream& operator <<(ostream &os, const Child &c);
+        friend istream& operator >>(istream &in, Child &c);
+
+    private:
+        string firstname;
+        string lastname;
+        int age;
+};
