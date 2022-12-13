60e5148fb1b005065838e0235350d397de4cb214
pmsese <120345253+pmsese@users.noreply.github.com>
2022-12-12 19:34:49 -0800

    done

--- Bank.cpp
+++ Bank.cpp
@@ -6,12 +6,12 @@ using namespace std;
 
 Bank::Bank(const string& file_name)
 {
-    //  To-do
+    ReadFile(file_name);
 }
 
 Bank::~Bank()
 {
-    //  To-do
+    account_list_.CleanUp();
 }
 
 bool Bank::ReadFile(const string& file_name)
@@ -120,7 +120,6 @@ void Bank::ProcessQueue()
                 cerr << "ERROR: " << transaction.account_id() "is an invalid account ID. Transaction refused.(Account IDs must be 4 digits)" << endl;
                 break;
 
-            //  To-do
             case 'T':
                 if (transaction.account_id() > 999 && transaction.account_id() < 10000)
                 {
@@ -129,6 +128,19 @@ void Bank::ProcessQueue()
                     bool initial = account_list_.Retrieve(transaction.account_id(), initialAccount);
                     bool final = account_list_.Retrieve(transaction.destination_account_id(), finalAccount);
                     if (intial && final)
+                    {
+                        if (initial.getUserBalance(transaction.fund_id) >= transaction.amount())
+                        {
+                            initialAccount.substractAcc(transaction.fund_id(), transaction.amount());
+                            finalAccount.Deposit(transaction.destination_fund_id(), transaction.destination_account_id());
+                            initialAccount.transferRecord(transaction.fund_id(), transaction);
+                            finalAccount.transferRecord(transaction.destination_fund_id(), transaction);
+                        }
+                        else
+                        cerr << "ERROR: Fund's balance is too low to transfer amount. Transaction refused." << endl;
+                    }
+                    else
+                    cerr << "ERROR: Account(s) not found." << endl;
                 }
                 else
                 cerr << "ERROR: " << transaction.account_id() "is an invalid account ID. Transaction refused.(Account IDs must be 4 digits)" << endl;
